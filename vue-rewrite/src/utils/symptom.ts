bcd26dd73d0f6edeebfe685aa0640c430d3e3c74
Clara <honjes@protonmail.com>
2024-09-05 23:22:32 +0200

    add util functions to create DayView and SymtpomOverview

--- vue-rewrite/src/utils/symptom.ts
+++ vue-rewrite/src/utils/symptom.ts
@@ -1,4 +1,6 @@
-import { ISymptomLog } from "@/types/symptom"
+import { ContentType, DayView, IDay } from "@/types/day"
+import { LogTypes } from "@/types/log"
+import { ISymptom, ISymptomLog, ISymptomOverview } from "@/types/symptom"
 
 /**
  * Creates a symptom log
@@ -9,4 +11,40 @@ import { ISymptomLog } from "@/types/symptom"
  */
 export function buildISymptomLog(time: string, pain: number, detail: string): ISymptomLog {
   return { time, pain, detail }
+}
+
+/**
+ * Creates a DayView object from a Day object
+ * @param day - Day object
+ * @returns DayView
+ */
+export function buildDayView(day: IDay): DayView {
+  const content: ContentType[] = [...day.logs, ...day.meds, ...day.meals]
+  console.log(content)
+  day.symptoms.forEach(s => content.push(...buildSymptomOverview(s)))
+  console.log(content)
+  content.sort((a, b) => a.time.localeCompare(b.time))
+
+  return {
+    wakeUp: day.wakeUp,
+    goToBed: day.goToBed,
+    content,
+    symptoms: [...day.symptoms],
+  }
+}
+
+/**
+ * Creates a SymptomOverview object from a Symptom object
+ * @param symptom - Symptom object
+ * @returns SymptomOverview
+ */
+export function buildSymptomOverview(symptom: ISymptom): ISymptomOverview[] {
+  const returnArray: ISymptomOverview[] = symptom.logs.map(log => ({
+    type: LogTypes.symptoms,
+    label: symptom.label,
+    key: symptom.key,
+    time: log.time,
+    pain: log.pain,
+  }))
+  return returnArray
 }
