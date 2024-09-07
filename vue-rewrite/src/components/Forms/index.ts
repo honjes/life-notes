598063bec78cf650dfbe240dda0e8d6aa3f24589
Clara <honjes@protonmail.com>
2024-09-07 12:32:11 +0200

    add a collected export for form components

---
+++ vue-rewrite/src/components/Forms/index.ts
@@ -0,0 +1,5 @@
+import AddASymptom from "./AddASymptom.vue"
+import AddAMeal from "./AddAMeal.vue"
+import AddWakeUpGoToBed from "./AddWakeUpGoToBed.vue"
+
+export { AddASymptom, AddAMeal, AddWakeUpGoToBed }
