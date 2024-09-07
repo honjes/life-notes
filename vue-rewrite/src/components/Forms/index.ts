2a4471016bcfedb5e9cf37eaf1014e465bb95802
Clara <honjes@protonmail.com>
2024-09-07 15:14:33 +0200

    add form for the AddAMed modal

--- vue-rewrite/src/components/Forms/index.ts
+++ vue-rewrite/src/components/Forms/index.ts
@@ -2,5 +2,6 @@ import AddASymptom from "./AddASymptom.vue"
 import AddAMeal from "./AddAMeal.vue"
 import AddWakeUpGoToBed from "./AddWakeUpGoToBed.vue"
 import CreateEditSymptom from "./CreateEditSymptom.vue"
+import AddAMed from "./AddAMed.vue"
 
-export { AddASymptom, AddAMeal, AddWakeUpGoToBed, CreateEditSymptom }
+export { AddASymptom, AddAMeal, AddWakeUpGoToBed, CreateEditSymptom, AddAMed }
