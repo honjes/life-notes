464b9a7b59049ae24ce7c2252e9c6058b747c146
Clara <honjes@protonmail.com>
2024-09-08 15:17:40 +0200

    add a component for the noteForm

--- vue-rewrite/src/components/Forms/index.ts
+++ vue-rewrite/src/components/Forms/index.ts
@@ -3,5 +3,6 @@ import AddAMeal from "./AddAMeal.vue"
 import AddWakeUpGoToBed from "./AddWakeUpGoToBed.vue"
 import CreateEditSymptom from "./CreateEditSymptom.vue"
 import AddAMed from "./AddAMed.vue"
+import AddANote from "./AddANote.vue"
 
-export { AddASymptom, AddAMeal, AddWakeUpGoToBed, CreateEditSymptom, AddAMed }
+export { AddASymptom, AddAMeal, AddWakeUpGoToBed, CreateEditSymptom, AddAMed, AddANote }
