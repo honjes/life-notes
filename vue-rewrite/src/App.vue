8f82b5dd9abdf7b6d2b125e5ae869f7012a89e14
Clara <honjes@protonmail.com>
2024-09-10 11:40:26 +0200

    initalise mainStore in app Component

--- vue-rewrite/src/App.vue
+++ vue-rewrite/src/App.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from "@ionic/vue"
+import { useMainStore } from "./store"
+import { onBeforeMount } from "vue"
+
+// External Components
+const mainStore = useMainStore()
+
+// Init
+// initalise main store for settings
+if (!mainStore.initalised) {
+  onBeforeMount(() => {
+    const unsubscribe = mainStore.$subscribe(() => {
+      if (mainStore.initalised) {
+        unsubscribe()
+      }
+    })
+  })
+}
 </script>
 
 <template>
