051b1e300f3bb65d3fdb2e3931ca777dcd910c7e
Clara <honjes@protonmail.com>
2024-09-10 11:38:21 +0200

    add language switch in mainStore

--- vue-rewrite/src/store/mainStore.ts
+++ vue-rewrite/src/store/mainStore.ts
@@ -1,34 +1,48 @@
 import { ISymptom } from "@/types"
 import { defineStore } from "pinia"
 import { ref } from "vue"
+import { useI18n } from "vue-i18n"
+
+export enum Languages {
+  FR = "fr",
+  EN = "en",
+}
 
 export interface ISettings {
   defaultSymptom: string
+  language: Languages
 }
 
 const settingsDefault: ISettings = {
   defaultSymptom: "none",
+  language: Languages.EN,
 }
 
 export const useMainStore = defineStore("main", () => {
   const db = new PouchDB<ISettings>("main")
 
   const settingsDocId = "settings"
+  const initalised = ref(false)
   const settings = ref<ISettings>(settingsDefault)
+  const { locale } = useI18n()
 
   // init settings
   db.get<ISettings>(settingsDocId)
     .then(settingsResponse => {
       if (settingsResponse) {
-        settings.value.defaultSymptom = settingsResponse.defaultSymptom || "none"
+        settings.value = settingsResponse
       }
+      initalised.value = true
     })
     .catch(_ => {
+      initalised.value = true
+      // save default settings to db
       updateSettings()
     })
 
   async function updateSettings() {
     try {
+      console.log("update settings: ", settings.value)
       await db.upsert(settingsDocId, () => ({ ...settings.value }))
     } catch (err) {
       console.error("update settings error: ", err)
@@ -45,5 +59,16 @@ export const useMainStore = defineStore("main", () => {
     await updateSettings()
   }
 
-  return { settings, setDefaultSymptom }
+  /**
+   * sets the language
+   * @param {Languages} language - language to set
+   */
+  async function setLanguage(language: Languages) {
+    settings.value.language = language
+    locale.value = language
+
+    await updateSettings()
+  }
+
+  return { settings, initalised, setDefaultSymptom, setLanguage }
 })
