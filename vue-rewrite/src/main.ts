d1269910b7a9afde0ddc2d5b19c58952d32393c7
Honjes <honjes@protonmail.com>
2024-10-06 22:44:25 +0200

    move normalize css to a layer

--- vue-rewrite/src/main.ts
+++ vue-rewrite/src/main.ts
@@ -13,7 +13,6 @@ import { IonicVue } from "@ionic/vue"
 import "@ionic/vue/css/core.css"
 
 /* Basic CSS for apps built with Ionic */
-import "@ionic/vue/css/normalize.css"
 import "@ionic/vue/css/structure.css"
 import "@ionic/vue/css/typography.css"
 
