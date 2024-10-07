8f1910dcee54a534a6a5f9f6c8635e6ade78fef8
Honjes <honjes@protonmail.com>
2024-10-07 11:56:27 +0200

    update styling of Detailed Dialog

--- vue-rewrite/src/components/DetailedDataDialog.vue
+++ vue-rewrite/src/components/DetailedDataDialog.vue
@@ -49,57 +49,57 @@ onBeforeMount(() => {
       <div v-if="data.type === DataTypes.symptoms">
         <div class="flex flex-row gap-2">
           <div class="w-1/2 font-bold">{{ t("DATE") }}:</div>
-          <div class="w-1/2">{{ format(new Date(`${date} ${data.time}`), `${dateFormat} ${timeFormat}`) }}</div>
+          <div class="w-full">{{ format(new Date(`${date} ${data.time}`), `${dateFormat} ${timeFormat}`) }}</div>
         </div>
         <div class="flex flex-row gap-2">
           <div class="w-1/2 font-bold">{{ t("SYMPTOM") }}:</div>
-          <div class="w-1/2">{{ (data as ISymptomOverview).label }}</div>
+          <div class="w-full">{{ (data as ISymptomOverview).label }}</div>
         </div>
         <div class="flex flex-row gap-2">
           <div class="w-1/2 font-bold">{{ t("PAIN") }}:</div>
-          <div class="w-1/2">{{ (data as ISymptomOverview).pain }}/5</div>
+          <div class="w-full">{{ (data as ISymptomOverview).pain }}/5</div>
         </div>
         <div class="flex flex-row gap-2">
           <div class="w-1/2 font-bold">{{ t("DETAIL") }}:</div>
-          <div class="w-1/2">{{ (data as ISymptomOverview).detail }}</div>
+          <div class="w-full">{{ (data as ISymptomOverview).detail }}</div>
         </div>
       </div>
       <div v-else-if="data.type === DataTypes.meds">
         <div class="flex flex-row gap-2">
           <div class="w-1/2 font-bold">{{ t("DATE") }}:</div>
-          <div class="w-1/2">{{ format(new Date(`${date} ${data.time}`), `${dateFormat} ${timeFormat}`) }}</div>
+          <div class="w-full">{{ format(new Date(`${date} ${data.time}`), `${dateFormat} ${timeFormat}`) }}</div>
         </div>
         <div class="flex flex-row gap-2">
           <div class="w-1/2 font-bold">{{ t("MED") }}:</div>
-          <div class="w-1/2">{{ (data as IMedOverview).key }}</div>
+          <div class="w-full">{{ (data as IMedOverview).key }}</div>
         </div>
         <div class="flex flex-row gap-2">
           <div class="w-1/2 font-bold">{{ t("QUANTITY") }}:</div>
-          <div class="w-1/2">{{ (data as IMedOverview).quantity }}</div>
+          <div class="w-full">{{ (data as IMedOverview).quantity }}</div>
         </div>
       </div>
       <div v-else-if="data.type === DataTypes.meals">
         <div class="flex flex-row gap-2">
           <div class="w-1/2 font-bold">{{ t("DATE") }}:</div>
-          <div class="w-1/2">{{ format(new Date(`${date} ${data.time}`), `${dateFormat} ${timeFormat}`) }}</div>
+          <div class="w-full">{{ format(new Date(`${date} ${data.time}`), `${dateFormat} ${timeFormat}`) }}</div>
         </div>
         <div class="flex flex-row gap-2">
           <div class="w-1/2 font-bold">{{ t("MEAL") }}:</div>
-          <div class="w-1/2">{{ (data as IMeal).key }}</div>
+          <div class="w-full">{{ (data as IMeal).key }}</div>
         </div>
         <div class="flex flex-row gap-2">
           <div class="w-1/2 font-bold">{{ t("DETAIL") }}:</div>
-          <div class="w-1/2">{{ (data as IMeal).detail }}</div>
+          <div class="w-full">{{ (data as IMeal).detail }}</div>
         </div>
       </div>
       <div v-else-if="data.type === DataTypes.note">
         <div class="flex flex-row gap-2">
           <div class="w-1/2 font-bold">{{ t("DATE") }}:</div>
-          <div class="w-1/2">{{ format(new Date(`${date} ${data.time}`), `${dateFormat} ${timeFormat}`) }}</div>
+          <div class="w-full">{{ format(new Date(`${date} ${data.time}`), `${dateFormat} ${timeFormat}`) }}</div>
         </div>
         <div class="flex flex-row gap-2">
           <div class="w-1/2 font-bold">{{ t("NOTE") }}:</div>
-          <div class="w-1/2">{{ (data as INoteOverview).detail }}</div>
+          <div class="w-full">{{ (data as INoteOverview).detail }}</div>
         </div>
       </div>
     </div>
